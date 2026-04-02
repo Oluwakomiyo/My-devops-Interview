6cfad9c534ceaf89dca2f23de39c98b51e462f6c
franklyn <oladeindefranklyn@gmail.com>
2026-04-02 15:49:49 +0100

    Modified frontend, backend and docker-cmpose

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,6 @@ app.get('/health', (req, res) => {
 
 app.use('/api', messageRoute);
 
-app.listen(3000, () => {
+app.listen(3000, '0.0.0.0', () => {
   console.log('Server running on port 3000');
 });
